81e647420dee22ea7d51848f76e2002e6b0862cb
Ryan Thissen <rmt1855@gmail.com>
2018-10-05 19:53:48 -0700

    working authentication app

--- App.js
+++ App.js
@@ -1,32 +1,71 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './src/Components/Common';
+import { Header, Button, Spinner } from './src/Components/Common';
 import { firebaseConfig } from './firebaseconfig';
 import LoginForm from './src/Components/LoginForm';
 
 
 export default class App extends React.Component {
+
+  state = {
+    loggedIn: null,
+  }
   componentWillMount() {
     console.log(firebaseConfig, 'firebaseonfig')
     firebase.initializeApp(firebaseConfig)
+    
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   };
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true: 
+        return (
+          <View style={styles.logoutButtonStyle}>
+            <Button onPress={() => firebase.auth().signOut()}>Log out</Button>
+          </View>
+        );
+      case false:
+        return <LoginForm />
+      
+      default: 
+        return (
+          <View style={styles.spinnerStyle}>
+            <Spinner size='large' />
+          </View>
+        )
+    }
+    
+    
+    return <LoginForm />;
+  }
+
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+const styles = {
+  spinnerStyle: {
+    paddingTop: 50,
     justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+  logoutButtonStyle: {
+    flexDirection: 'row',
+    paddingTop: 5
+  }
+};
